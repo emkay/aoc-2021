659db5ff2c3d86104c2add47e59e5741bd587aab
Michael Matuzak <mmatuzak@gmail.com>
2021-12-01 22:27:07 -0800

    day 1 complete

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,29 @@ fn main() {
     let s = fs::read_to_string(filepath)
         .expect("Something went wrong reading the file");
 
-    let v: Vec<u16> = s.split_terminator("\n")
+    let v: Vec<u16> = s
+        .split_terminator("\n")
         .map(|x| x.parse::<u16>().unwrap())
         .collect();
 
     let mut increase_count = 0;
+    let mut current_sum: u16;
+    let mut prev_sum: u16 = 0;
 
-    let iter = v.windows(2);
+    let iter = v.windows(3);
     for items in iter {
-        let item = items[0];
-        let next = items[1];
+        current_sum = items.iter().sum();
+        
+        increase_count = match prev_sum {
+            ps if ps < current_sum => increase_count + 1,
+            _ => increase_count
+        };
 
-        if item <= next {
-            increase_count += 1;
-        }
+        prev_sum = current_sum;
     }
-
+    
+    // the first iteration will always give you an extra increase because there was no previous sum
+    // and prev_sum is set to 0.
+    increase_count -= 1;
     println!("Increases: {}", increase_count);
 }
