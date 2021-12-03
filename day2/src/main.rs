b62a6bbe896c4ff30609ec1bba84d6acceb9061e
Mike Matuzak <mmatuzak@gmail.com>
2021-12-02 16:49:19 -0800

    day 2 part i

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let filepath = Path::new("./data/day2-input.txt");
+
+    let s = fs::read_to_string(filepath)
+        .expect("Something went wrong reading the file");
+
+    let v: Vec<Vec<&str>> = s
+        .split_terminator("\n")
+        .map(|x| x.split(" ").collect())
+        .collect();
+
+    let mut horizontal_pos = 0;
+    let mut depth_pos = 0;
+
+    for item in v.iter() {
+        let command = item[0];
+        let value = item[1].parse::<u32>().unwrap();
+
+        match command {
+            "forward" => horizontal_pos += value,
+            "down" => depth_pos += value,
+            "up" => depth_pos -= value,
+            _ => panic!("nope")
+        }
+    }
+
+    let answer = horizontal_pos * depth_pos;
+    println!("answer: {}", answer);
 }
